7e7f14d3b1aa4face40a2b38aa3dd3ed2be6a8b8
suchesr2005-gif <suches.r2005@gmail.com>
2026-02-07 11:36:53 +0800

    hapus

--- uts 6/part 2/cli.py
+++
@@ -1,3 +0,0 @@
-from main import run_program
-
-run_program()
--- uts 7/UTS 7 part2/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def pause():
-    input("\nTekan Enter untuk melanjutkan...")
-    clear()
